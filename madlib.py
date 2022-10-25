ae62cbe1b9471a1723003be3ff59f76d1cee93fb
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-10-25 10:07:08 -0400

    practice

---
+++ madlib.py
@@ -0,0 +1,14 @@
+# string concatenation ( how to put strings together)
+#football_team = "Steelers"
+#print("the best team is" + football_team)
+#print("the best team is{}".format(football_team))
+#print(f"the best team is{football_team}")
+adj = input("Adjective:")
+adj2 = input("Adjective:")
+num = input(int())
+noun = input("noun")
+noun2 = input("noun")
+madlib = f"Working in a kitchen is so {adj}! You get to stand on your feet for {num} hours a day,\
+Ungrateful {noun} requesting silly things, and of course {adj2} co_workers.\
+    The best part is when you are finished you come home smelling like{noun}!"
+print(madlib)
