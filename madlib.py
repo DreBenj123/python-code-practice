6a3ad6e5f13664d2defb6fa047cdeec846de360f
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-10-27 08:15:01 -0400

    more practice

--- madlib.py
+++ madlib.py
@@ -4,11 +4,11 @@
 #print("the best team is{}".format(football_team))
 #print(f"the best team is{football_team}")
 adj = input("Adjective:")
-adj2 = input("Adjective:")
+noun2 = input("noun:")
 num = input(int())
-noun = input("noun")
-noun2 = input("noun")
+noun = input("noun:")
+adj2 = input("Adjective:")
 madlib = f"Working in a kitchen is so {adj}! You get to stand on your feet for {num} hours a day,\
 Ungrateful {noun} requesting silly things, and of course {adj2} co_workers.\
-    The best part is when you are finished you come home smelling like{noun}!"
+    The best part is when you are finished you come home smelling like{noun2}!"
 print(madlib)
